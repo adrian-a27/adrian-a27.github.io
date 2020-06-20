531f09c3bab661d667db7a3f0509801b249ebf7c
Glitch (hello-webpage) <none>
2020-06-20 16:12:57 +0000

    🌪🛫 Checkpoint ./app.scss:5887444/27 ./package.json:5887444/5132 ./index.html:5887444/192 ./webpack.config.js:5887444/5819 ./my-theme.scss:5887444/137 ./my-components.scss:5887444/669

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 module.exports = [{
-  entry: './app.scss',
+  entry: ['./app.scss', './app.js'],
   output: {
-    // This is necessary for webpack to compile
-    // But we never use style-bundle.js
-    filename: 'style-bundle.js',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -19,20 +17,20 @@ module.exports = [{
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
           {
-  loader: 'sass-loader',
-  options: {
-    // Prefer Dart Sass
-    implementation: require('sass'),
-
-    // See https://github.com/webpack-contrib/sass-loader/issues/804
-    webpackImporter: false,
-    sassOptions: {
-      includePaths: ['./node_modules']
-    },
-  }
-},
-          },
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules']
+            }
+          }
         ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-assign']
+        },
       }
     ]
   },
